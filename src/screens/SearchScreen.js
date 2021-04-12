c4bd8d8bfbcdc806715e1b754ae3eeb39e49cc05
Cory Bass <cory.bass.07@gmail.com>
2021-04-12 12:38:37 -0500

    extracted yelp api call to seperate file for use as custom hook

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,40 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { View, StyleSheet, Text } from "react-native";
 
-// axios
-import yelp from "../api/yelp";
+import useResults from "../hooks/useResults";
 
 // local files
 import SearchBar from "../components/SearchBar";
 
 const SearchScreen = () => {
     const [term, setTerm] = useState("");
-    const [results, setResults] = useState([]);
-    const [errorMessage, setErrorMessage] = useState("");
-
-    const searchApi = async () => {
-        try {
-            const response = await yelp.get("/search", {
-                params: {
-                    limit: 50,
-                    term,
-                    location: "san jose",
-                },
-            });
-            setResults(response.data.businesses);
-        } catch (err) {
-            setErrorMessage("Something went wrong");
-            console.error("YELP API ERROR:", err);
-            return;
-        }
-    };
+    const [searchApi, results, errorMessage] = useResults();
 
     return (
         <View>
             <SearchBar
                 term={term}
                 onTermChange={setTerm}
-                onTermSubmit={searchApi}
+                onTermSubmit={() => searchApi(term)}
             />
             <Text>Search Screen</Text>
             {errorMessage.length > 0 ? (
